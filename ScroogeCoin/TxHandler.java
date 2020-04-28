ec1bc7eadaa3efb8a09a1d49d897280dbb13383e
Andrei Tanase <itashi987@gmail.com>
2020-04-28 15:19:31 +0200

    Pusher comented stuff
    skuffed english

--- ScroogeCoin/TxHandler.java
+++ ScroogeCoin/TxHandler.java
@@ -7,11 +7,11 @@ public class TxHandler {
      * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
      * constructor.
      */
-    public UTXOPool _utxoPool;
+    public UTXOPool _utxoPool; // Creating a variable called utxoPool that will be used as a copy for the methods bellow.
 
     public TxHandler(UTXOPool utxoPool) {
         _utxoPool = new UTXOPool(utxoPool);
-    }
+    }  // creating the copy of the variable utxoPool
 
     /**
      * @return true if:
@@ -25,20 +25,20 @@ public class TxHandler {
     public boolean isValidTx(Transaction tx) {
         double sumInput = 0;
         double sumOutput = 0;
-        ArrayList<UTXO> usedUTXO = new ArrayList<>();
+        ArrayList<UTXO> usedUTXO = new ArrayList<>(); // Creates an arraylist that stores all the usedUTXO
 
-        // Iterate all inputs in tx
+        // Creates a for loop that will iterate through the different inputs in the transactions
         for (int i = 0; i < tx.numInputs(); i++){
 
-            // Make a copy of the current input in tx
+            // Making a copy of the current transaction input.
             Transaction.Input input = tx.getInput(i);
 
-            // make a new UTXO using the previous hash and the current output index.
+            // Creating a new UTXO that uses the previous transaction hash H() and the current output index.
             UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
 
             // Check rule 1: All outputs claimed by {@code tx} are in the current UTXO pool
 
-            // Check if the utxo pool copy contains the current utxo.
+            // If statement that check the copy of utxoPool for already containing a utxo. if yes then return false.
             if (!_utxoPool.contains(utxo)) return false;
 
             // Check rule 2: The signatures on each input of {@code tx} are valid
@@ -47,23 +47,23 @@ public class TxHandler {
             Transaction.Output output = _utxoPool.getTxOutput(utxo);
 
             // Get raw data to be used in verifySignature
-            byte[] message = tx.getRawDataToSign(i);
-            if (!Crypto.verifySignature(output.address, message, input.signature)) return false;
+            byte[] message = tx.getRawDataToSign(i); // Creates an byte array that stores the raw message data.
+            if (!Crypto.verifySignature(output.address, message, input.signature)) return false; // If statement that checks the validity of the signature based on previous transactions
 
             // Check rule 3: No UTXO is claimed multiple times by {@code tx}
-            if (usedUTXO.contains(utxo)) return false;
-            usedUTXO.add(utxo);
+            if (usedUTXO.contains(utxo)) return false; // If statement that checks for rule nr 3. This prevents double spending
+            usedUTXO.add(utxo); // If utxo is not previously used it is added to the usedUTXO ArrayList
             sumInput += output.value;
         }
 
         // Check rule 4: All of {@code tx}s output values are non-negative
 
-        // Iterate all outputs in tx
+        // Create a new for loop that iterates through all the outputs in a transaction
         for (int i = 0; i < tx.numOutputs(); i++){
-            // Make a copy of the current output in tx
+            // Creates a copy of the current output handled in the transaction
             Transaction.Output output = tx.getOutput(i);
-            if (output.value < 0) return false;
-            sumOutput += output.value;
+            if (output.value < 0) return false; // If statement that checks if the output value of coin is below 0, is yes than something is wrong
+            sumOutput += output.value; //Add the remaining value from the output to the sumOutput variable.
         }
 
         // Check rule 5: The sum of {@code tx}s input values is greater than or equal to the sum of its output values; and false otherwise.
@@ -81,18 +81,18 @@ public class TxHandler {
         // A buffer for accepted Txs, contains only valid Txs.
         ArrayList<Transaction> validTxs = new ArrayList<>();
 
-        // Iterate through each tx
+        // // Create a for loop that will iterate through all transactions in the list.
         for (Transaction t : possibleTxs){
             if (isValidTx(t)) {
                 validTxs.add(t);
 
-                // remove utxo's
+                // If statement that checks if the possible transaction t is valid, if not then removes the transaction from the array list created above
                 for (Transaction.Input input : t.getInputs()){
                     UTXO utxo = new UTXO(input.prevTxHash, input.outputIndex);
                     _utxoPool.removeUTXO(utxo);
                 }
 
-                // add new utxo
+                // Create a new byte hashmap that will store the hash of the current possible transaction.
                 byte[] hash = t.getHash();
                 for (int i = 0; i < t.numOutputs(); i++){
                     UTXO utxo = new UTXO(hash, i);
